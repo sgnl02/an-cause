9a1758ab8d280c0806451ca718a23f923c69ed2e
Stefan Jonker <programmer@stefanjonker.nl>
2014-07-08 00:27:13 +0200

    Added Grunt files for automation

---
+++ js/appcache.js
@@ -0,0 +1,11 @@
+var appCache = window.applicationCache;
+if (window.applicationCache) {
+	// See also: chrome://appcache-internals/
+	//console.log(window.applicationCache);
+
+  //applicationCache.addEventListener('updateready', function() {
+  //    if (confirm('An update is available. Reload now?')) {
+  //        window.location.reload();
+  //    }
+  //});
+}
